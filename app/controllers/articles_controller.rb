d580f51320c60c8941e81474ed70cef2c56602ce
flyyuan <740004544@qq.com>
2022-10-24 17:08:36 +0800

    feat: finish https://guides.rubyonrails.org/getting_started.html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -41,8 +44,9 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 end
